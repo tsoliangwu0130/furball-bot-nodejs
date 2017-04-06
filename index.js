a17f83b3e625003b28981fd9f31d2d4e301fdf23
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-06 14:11:39 -0700

    Update verify_token

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] ==='Aha_Moment_Labs') {
+    if (req.query['hub.verify_token'] ==='Furball_Bot') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
