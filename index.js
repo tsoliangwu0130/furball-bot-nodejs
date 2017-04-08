537a24c87e2c7efa1cce6b53beaf02a1a2c41d71
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-07 23:31:25 -0700

    Update template

--- index.js
+++ index.js
@@ -88,25 +88,34 @@ function sendGenericMessage(sender) {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Furball_01",
-                    "subtitle": "Furball_01",
+                    "title": "猜猜我在想什麼？",
+                    "subtitle": "",
                     "image_url": "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/17349777_408691256150251_3161583191849637124_o.jpg?oh=6e75b3b539cfd5b40053fa850a8442e5&oe=598BAA7F",
                     "buttons": [{
-                        "type":"element_share",
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/pg/furballchubbymeow/photos/?ref=page_internal",
+                        "title":"View More",
+                        "webview_height_ratio": "compact"
                     }],
                 }, {
-                    "title": "Furball_02",
-                    "subtitle": "Furball_02",
+                    "title": "擠？我才不覺得咧喵～",
+                    "subtitle": "",
                     "image_url": "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/16107142_378661272486583_6814191021096657673_o.jpg?oh=7161ba75c77aacada15b551a2f99b7ad&oe=599A2E77",
                     "buttons": [{
-                        "type":"element_share",
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/pg/furballchubbymeow/photos/?ref=page_internal",
+                        "title":"View More",
+                        "webview_height_ratio": "compact"
                     }],
                 },  {
-                    "title": "Furball_03",
-                    "subtitle": "Furball_03",
+                    "title": "大家早安呀！（呵欠）",
+                    "subtitle": "",
                     "image_url": "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/15800282_371563219863055_2270307156689436092_o.jpg?oh=36e3ea8551491f96587140231fbb0f3d&oe=594E9C7D",
                     "buttons": [{
-                        "type":"element_share",
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/pg/furballchubbymeow/photos/?ref=page_internal",
+                        "title":"View More",
+                        "webview_height_ratio": "compact"
                     }],
                 }]  
             } 
