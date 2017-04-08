91c70f5f437bde895180e872d225d7743eb38679
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-07 17:48:56 -0700

    Update code

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.listen(app.get('port'), function() {
 
 
 // API end point
-
 app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
@@ -82,7 +81,6 @@ function sendTextMessage(sender, text) {
 
 
 // Send an test message back as two cards.
-
 function sendGenericMessage(sender) {
     messageData = {
         "attachment": {
@@ -91,18 +89,21 @@ function sendGenericMessage(sender) {
                 "template_type": "generic",
                 "elements": [{
                     "title": "Furball_01",
+                    "subtitle": "Furball_01",
                     "image_url": "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/17349777_408691256150251_3161583191849637124_o.jpg?oh=6e75b3b539cfd5b40053fa850a8442e5&oe=598BAA7F",
                     "buttons": [{
                         "type":"element_share",
                     }],
                 }, {
                     "title": "Furball_02",
+                    "subtitle": "Furball_02",
                     "image_url": "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/16107142_378661272486583_6814191021096657673_o.jpg?oh=7161ba75c77aacada15b551a2f99b7ad&oe=599A2E77",
                     "buttons": [{
                         "type":"element_share",
                     }],
                 },  {
                     "title": "Furball_03",
+                    "subtitle": "Furball_03",
                     "image_url": "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/15800282_371563219863055_2270307156689436092_o.jpg?oh=36e3ea8551491f96587140231fbb0f3d&oe=594E9C7D",
                     "buttons": [{
                         "type":"element_share",
